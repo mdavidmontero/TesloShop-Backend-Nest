216dbc5ab94c5a9caf3a2fe3760252a2ae039b2e
Mateo Rodriguez <mdavidmontero6@gmail.com>
2024-12-19 13:00:01 -0500

    Rest api

---
+++ README.md
@@ -0,0 +1,27 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+</p>
+
+# Teslo Api
+
+1. Clonar proyecto
+2. Instalar dependencias
+
+```
+npm install
+```
+
+3. Clonar el archivo .env.template en .env
+4. Editar el archivo .env
+5. Levantar la base de datos
+
+```bash
+docker-compose up -d
+```
+
+6. Levantar:
+
+```bash
+   npm run start:dev
+
+```
---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ProductsModule,
+
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
